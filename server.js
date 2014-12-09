d886b3a2e7c5d05da4723114ec127096d7c40634
Dima <tyurin94@gmail.com>
2014-12-09 18:40:26 +0400

    laba ready to start

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.get('*', function (req, res) {
+  res.send('404');
 });
 
 app.listen(app.get('port'), function() {
