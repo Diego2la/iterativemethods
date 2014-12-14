1535acc963a8af90069a1b420f471564bc431993
Dima <tyurin94@gmail.com>
2014-12-15 02:30:03 +0400

    Chebishov is ready

--- public/app/app.js
+++ public/app/app.js
@@ -7,72 +7,66 @@ app.config(function($locationProvider) {
     });
 });
 
-app.controller('Lab1Ctrl', function($scope, matrixStorage){
+app.controller('Lab1Ctrl', function($scope, matrixSolver){
     $scope.nMin = 3;
     $scope.nMax = 7;
     $scope.matrixIsHidden = true;
-    $scope.generate = function() {
+
+    $scope.solve = function() {
         if (isNaN($scope.nMin) || isNaN($scope.nMax) || $scope.nMin > $scope.nMax || $scope.nMin < 1) {
             alert('Wrong sizes');
             return;
         }
-        matrixStorage.generateMatrix($scope.nMin, $scope.nMax);
-        $scope.matrix = matrixStorage.getMatrix();
-        $scope.select($scope.matrix[0]);
+        $scope.matrix = matrixSolver.solve($scope.nMin, $scope.nMax);
+        $scope.select($scope.nMin);
         $scope.matrixIsHidden = false;
     }
-    $scope.select = function(m) {
-        $scope.selected = m;
-        matrixStorage.setSelected(m.n);
+
+    $scope.select = function(n) {
+        for (var i = 0; i < $scope.matrix.length; i++) {
+            if ($scope.matrix[i].n === n) {
+                $scope.sel = $scope.matrix[i];
+            }
+        }
     }
+
     $scope.getTabClass = function(n) {
         var res = 'list-group-item table-tab ';
-        if ($scope.selected.n === n) {
+        if ($scope.sel.n === n) {
             res += 'active';
         }
         return res;
     }
 });
 
-app.factory('matrixStorage', function($window) {
-    var nMin, nMax, nSelect;
-    var matrix = new Array();
+app.factory('matrixSolver', function($window) {
     var num = $window.numeric;
-
     var MIN = -100;
     var MAX = 100;
-
-    function isPositiveDet(A, n) {
-        var B = Array(n);
-        for (var i = 0; i < n; i++) {
-            B[i] = Array(n);
-            for (var j = 0; j < n; j++) {
-                B[i][j] = A[i][j];
-            }
-        }
-        return (numeric.det(B) > 0);
-    }
 
     function getRandInt() {
         var val = 0;
         while (val == 0) {
-            val = Math.floor(Math.random() * (MAX - MIN + 1)) + MIN;
+            val = (Math.floor(Math.random() * (MAX - MIN + 1)) + MIN) / 10;
         }
         return val;
     }
 
+    function norm(xNext, xPrev) {
+        var sum = 0;
+        for (var i = 0; i < xNext.length; i++) {
+            sum = sum + (xNext[i] - xPrev[i]) * (xNext[i] - xPrev[i]);
+        }
+        return Math.sqrt(sum);
+    }
+
     function generateMatrixA(n) {
-        // A - симметричная и положительно определенная матрица
         var A = new Array(n);
         for (var i = 0; i < n; i++) {
             A[i] = new Array(n);
-        }
-        for (var i = 0; i < n; i++) {
-            do {
-                for (var j = 0; j < i + 1; j++) {
-                    A[i][j] = A[j][i] = getRandInt();
-                }
-            } while (!isPositiveDet(A, i + 1));
+            for (var j = 0; j < n; j++) {
+                A[i][j] = getRandInt();
+            }
         }
         return A;
     }
@@ -95,136 +89,117 @@ app.factory('matrixStorage', function($window) {
                     M[i][j] = 0;
                 } else {
                     M[i][j] = getRandInt();
+                    M[i][j] = M[i][j] * M[i][j];
                 }
             }
         }
         return M;
     }
 
+    // Неявный метод скорейшего спуска
+    function resolveDescent(m) {}
+
+    // Неявный метод с Чебышевским набором параметров
+    function resolveChebyshev(m) {
+        var n = m.n;
+        var A = m.A;
+        var invM = m.invM;
+        var b = m.b;
+        console.log('n = ' + JSON.stringify(n));
+        console.log('A = ' + JSON.stringify(A));
+        console.log('invM = ' + JSON.stringify(invM));
+        console.log('b = ' + JSON.stringify(b));
+
+        var C = num.dot(invM, A);
+        var eigenvalues = num.eig(C).lambda.x;
+        var eigenMin = Math.min.apply(Math, eigenvalues);
+        var eigenMax = Math.max.apply(Math, eigenvalues);
+        console.log('C = ' + JSON.stringify(C));
+        console.log('eigenvalues = ' + JSON.stringify(eigenvalues));
+
+        var eps = eigenMin / eigenMax;
+        console.log('eps = ' + eps);
+        var ro0 = (1 - eps) / (1 + eps);
+        console.log('ro0 = ' + ro0);
+
+        var t = function (k) {
+            return Math.cos((2 * k - 1) * Math.PI / (2 * n));
+        };
+
+        var T0 = 2 / (eigenMin + eigenMax);
+        var T = function (k) {
+            return T0 / (1 + ro0 * t(k));
+        };
+
+        var start = new Date().getTime();
+        var xPrev, xNext = b;
+        var k = 0;
+        var normVal = 0;
+        do{
+            xPrev = xNext;
+            xNext = num.sub(b, num.dot(A, xPrev));
+            xNext = num.mul(xNext, T(k + 1));
+            xNext = num.dot(invM, xNext);
+            xNext = num.add(xNext, xPrev);
+            k = k + 1;
+            normVal = norm(xPrev, xNext);
+            console.log('norm = ' + JSON.stringify(normVal));
+        } while(normVal > 0.01);
+        var end = new Date().getTime();
+        return {
+            res: xNext,
+            k: k,
+            time: end - start,
+            error: normVal
+        }
+    }
+
     return {
-        generateMatrix: function(nMin, nMax) {
-            this.nMax = nMax;
-            this.nMin = nMin;
-            matrix = new Array();
+        solve: function(nMin, nMax) {
+            var matrix = new Array();
             for (var i = nMin; i <= nMax; i++) {
+                var A = generateMatrixA(i);
+                var b = generateMatrixB(i);
+                var inv = num.transpose(A);
                 matrix.push({
                     n: i,
-                    A: generateMatrixA(i),
-                    b: generateMatrixB(i),
-                    M: generateMatrixM(i)
+                    A: num.dot(inv, A),
+                    b: num.dot(inv, b),
+                    invM: num.transpose(generateMatrixM(i))
                 });
+                var sel = i - nMin;
+                matrix[sel].chebyshev = resolveChebyshev(matrix[sel]);
+                console.log('!!!!!!!!!!!!!!!' + JSON.stringify(matrix[sel].chebyshev));
+                matrix[sel].descent = resolveDescent(matrix[sel]);
             }
-        }, getMatrix: function() {
             return matrix;
-        }, setSelected: function(n) {
-            nSelect = n;
-        }, getSelectedMatrix: function(type) {
-            for (var i = 0; i < matrix.length; i++) {
-                if (matrix[i].n === nSelect) {
-                    if (type === 'A') {
-                        return matrix[i].A;
-                    }
-                    if (type === 'b') {
-                        return matrix[i].b;
-                    }
-                    return null;
-                }
-            }
-            return null;
-        }, resolveChebyshev: function(A, b, n) {
-            //Делаем матриуц B : x = h + Bx
-            var B = new Array(n);
-            for (var i = 0; i < n; i++) {
-                B[i] = new Array(n);
-                for (var j = 0; j < n; j++) {
-                    if (i != j) {
-                        B[i][j] = - A[i][j] / A[i][i];
-                    } else {
-                        B[i][j] = 0;
-                    }
-                }
-            }
-            var C = numeric.dot(numeric.inv(B), A);
-            var Ceigenvalues = numeric.eig(C).lambda.x;
-            var eps = Ceigenvalues.min / Ceigenvalues.max;
-            var ro0 = (1 - eps) / (1 + eps);
-
-            var ro1 = (1 - math.sqrt(eps)) / (1 + math.sqrt(eps));
-            var nIt = ln(2 / eps) / ln(1 / ro1);
-            var t = function(k) {
-                return math.cos((2*k - 1)*Math.PI/(2*n))
-            };
-
-            var Aeigenvalues = numeric.eig(A).lambda.x;
-            var T0 = 2 / (Aeigenvalues.min + Aeigenvalues.max);
-            var T = function(k) {
-                return T0 / (1 + ro0 * t(k));
-            };
-
-            var xNext = b;
-            var k = 0;
-            while (k < nIt) {
-                xPrev = xNext;
-                xNext = numeric.inv(M);
-                xNext = numeric.dot(xNext, numeric.sub(b, numeric.dot(A, xPrev)));
-                xNext = numeric.dot(xNext, T(k+1));
-                xNext = numeric.dot(xNext, xPrev);
-                k = k + 1;
-            }
-            return {
-                time: 100
-
-
-            };
         }
     };
 });
 
-app.directive('matrixA', function() {
-  return {
-      restrict: 'E',
-      scope: {},
-      controller: function($scope, matrixStorage) {
-          $scope.getData = function() {
-              return matrixStorage.getSelectedMatrix('A');
-          }
-     },
-      templateUrl: 'partials/matrixA'
-  };
-});
-
-app.directive('matrixB', function() {
+app.directive('matrixTable', function() {
     return {
         restrict: 'E',
-        scope: {},
-        controller: function($scope, matrixStorage) {
-            $scope.getData = function() {
-                return matrixStorage.getSelectedMatrix('b');
-            }
+        scope: {
+            data: "="
         },
-        templateUrl: 'partials/matrixB'
+        templateUrl: 'partials/matrixTable',
+        link: function(scope, elem, attrs) {
+            console.log(scope.data);
+        }
     };
 });
 
 
-//function isPositiveDefinite(A) {
-//    var eigenvalues = numeric.eig(A).lambda.x;
-//    var positive = 0;
-//    for(var i = 0; i < eigenvalues.length; i++) {
-//        if(eigenvalues[i] > 0) {
-//            positive = positive + 1;
-//        }
-//    }
-//    return (positive == eigenvalues.length);
-//}
-
-
-
-//var n = req.params.size;
-//var gen = generateMatrix(n);
-//var A = gen['A'];
-////var b = gen['b'];
-////res.json(A);
-//res.write(JSON.stringify(A));
-//res.write(JSON.stringify(numeric.eig(A).lambda));
-//res.write(isPositiveDefinite(A, n) ? 'pos' : 'not pos');
+app.directive('matrixRow', function() {
+    return {
+        restrict: 'E',
+        scope: {
+            data: "="
+        },
+        templateUrl: 'partials/matrixRow',
+        link: function(scope, elem, attrs) {
+            console.log(scope.data);
+        }
+    };
+});
