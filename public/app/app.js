53c42f5f104c4a340215ea2d75141bd164ad5552
Dima <tyurin94@gmail.com>
2014-12-14 16:23:44 +0400

    metrix generator is ready :) yahooo

--- public/app/app.js
+++ public/app/app.js
@@ -1,18 +1,230 @@
-angular.module('app', ['ngResource', 'ngRoute']);
+var app = angular.module('app', ['ngResource', 'ngRoute']);
 
-angular.module('app').config(function($routeProvider, $locationProvider) {
+app.config(function($locationProvider) {
     $locationProvider.html5Mode({
         enabled: true,
         requireBase: false
     });
-    $routeProvider
-        .when('/', {templateUrl:'/partials/matrix', controller: 'mainCtrl'});
 });
 
-angular.module('app').controller('mainCtrl', function($scope, $window){
-    var c = $window.numeric.add([7,8,9],[10,1,2])
-    console.log(c);
-    $scope.myVar = c;
-
-
+app.controller('Lab1Ctrl', function($scope, matrixStorage){
+    $scope.nMin = 3;
+    $scope.nMax = 7;
+    $scope.matrixIsHidden = true;
+    $scope.generate = function() {
+        if (isNaN($scope.nMin) || isNaN($scope.nMax) || $scope.nMin > $scope.nMax || $scope.nMin < 1) {
+            alert('Wrong sizes');
+            return;
+        }
+        matrixStorage.generateMatrix($scope.nMin, $scope.nMax);
+        $scope.matrix = matrixStorage.getMatrix();
+        $scope.select($scope.matrix[0]);
+        $scope.matrixIsHidden = false;
+    }
+    $scope.select = function(m) {
+        $scope.selected = m;
+        matrixStorage.setSelected(m.n);
+    }
+    $scope.getTabClass = function(n) {
+        var res = 'list-group-item table-tab ';
+        if ($scope.selected.n === n) {
+            res += 'active';
+        }
+        return res;
+    }
 });
+
+app.factory('matrixStorage', function($window) {
+    var nMin, nMax, nSelect;
+    var matrix = new Array();
+    var num = $window.numeric;
+
+    var MIN = -100;
+    var MAX = 100;
+
+    function isPositiveDet(A, n) {
+        var B = Array(n);
+        for (var i = 0; i < n; i++) {
+            B[i] = Array(n);
+            for (var j = 0; j < n; j++) {
+                B[i][j] = A[i][j];
+            }
+        }
+        return (numeric.det(B) > 0);
+    }
+
+    function getRandInt() {
+        var val = 0;
+        while (val == 0) {
+            val = Math.floor(Math.random() * (MAX - MIN + 1)) + MIN;
+        }
+        return val;
+    }
+
+    function generateMatrixA(n) {
+        // A - симметричная и положительно определенная матрица
+        var A = new Array(n);
+        for (var i = 0; i < n; i++) {
+            A[i] = new Array(n);
+        }
+        for (var i = 0; i < n; i++) {
+            do {
+                for (var j = 0; j < i + 1; j++) {
+                    A[i][j] = A[j][i] = getRandInt();
+                }
+            } while (!isPositiveDet(A, i + 1));
+        }
+        return A;
+    }
+
+    function generateMatrixB(n) {
+        var b = new Array(n);
+        for (var i = 0; i < n; i++) {
+            b[i] = getRandInt();
+        }
+        return b;
+    }
+
+    function generateMatrixM(n) {
+        // Выбирая в качестве M какую-либо легко обратимую матрицу, получают общий неявный метод простой итерации.
+        var M = new Array(n);
+        for (var i = 0; i < n; i++) {
+            M[i] = new Array(n);
+            for (var j = 0; j < n; j++) {
+                if (i != j) {
+                    M[i][j] = 0;
+                } else {
+                    M[i][j] = getRandInt();
+                }
+            }
+        }
+        return M;
+    }
+
+    return {
+        generateMatrix: function(nMin, nMax) {
+            this.nMax = nMax;
+            this.nMin = nMin;
+            matrix = new Array();
+            for (var i = nMin; i <= nMax; i++) {
+                matrix.push({
+                    n: i,
+                    A: generateMatrixA(i),
+                    b: generateMatrixB(i),
+                    M: generateMatrixM(i)
+                });
+            }
+        }, getMatrix: function() {
+            return matrix;
+        }, setSelected: function(n) {
+            nSelect = n;
+        }, getSelectedMatrix: function(type) {
+            for (var i = 0; i < matrix.length; i++) {
+                if (matrix[i].n === nSelect) {
+                    if (type === 'A') {
+                        return matrix[i].A;
+                    }
+                    if (type === 'b') {
+                        return matrix[i].b;
+                    }
+                    return null;
+                }
+            }
+            return null;
+        }, resolveChebyshev: function(A, b, n) {
+            //Делаем матриуц B : x = h + Bx
+            var B = new Array(n);
+            for (var i = 0; i < n; i++) {
+                B[i] = new Array(n);
+                for (var j = 0; j < n; j++) {
+                    if (i != j) {
+                        B[i][j] = - A[i][j] / A[i][i];
+                    } else {
+                        B[i][j] = 0;
+                    }
+                }
+            }
+            var C = numeric.dot(numeric.inv(B), A);
+            var Ceigenvalues = numeric.eig(C).lambda.x;
+            var eps = Ceigenvalues.min / Ceigenvalues.max;
+            var ro0 = (1 - eps) / (1 + eps);
+
+            var ro1 = (1 - math.sqrt(eps)) / (1 + math.sqrt(eps));
+            var nIt = ln(2 / eps) / ln(1 / ro1);
+            var t = function(k) {
+                return math.cos((2*k - 1)*Math.PI/(2*n))
+            };
+
+            var Aeigenvalues = numeric.eig(A).lambda.x;
+            var T0 = 2 / (Aeigenvalues.min + Aeigenvalues.max);
+            var T = function(k) {
+                return T0 / (1 + ro0 * t(k));
+            };
+
+            var xNext = b;
+            var k = 0;
+            while (k < nIt) {
+                xPrev = xNext;
+                xNext = numeric.inv(M);
+                xNext = numeric.dot(xNext, numeric.sub(b, numeric.dot(A, xPrev)));
+                xNext = numeric.dot(xNext, T(k+1));
+                xNext = numeric.dot(xNext, xPrev);
+                k = k + 1;
+            }
+            return {
+                time: 100
+
+
+            };
+        }
+    };
+});
+
+app.directive('matrixA', function() {
+  return {
+      restrict: 'E',
+      scope: {},
+      controller: function($scope, matrixStorage) {
+          $scope.getData = function() {
+              return matrixStorage.getSelectedMatrix('A');
+          }
+     },
+      templateUrl: 'partials/matrixA'
+  };
+});
+
+app.directive('matrixB', function() {
+    return {
+        restrict: 'E',
+        scope: {},
+        controller: function($scope, matrixStorage) {
+            $scope.getData = function() {
+                return matrixStorage.getSelectedMatrix('b');
+            }
+        },
+        templateUrl: 'partials/matrixB'
+    };
+});
+
+
+//function isPositiveDefinite(A) {
+//    var eigenvalues = numeric.eig(A).lambda.x;
+//    var positive = 0;
+//    for(var i = 0; i < eigenvalues.length; i++) {
+//        if(eigenvalues[i] > 0) {
+//            positive = positive + 1;
+//        }
+//    }
+//    return (positive == eigenvalues.length);
+//}
+
+
+
+//var n = req.params.size;
+//var gen = generateMatrix(n);
+//var A = gen['A'];
+////var b = gen['b'];
+////res.json(A);
+//res.write(JSON.stringify(A));
+//res.write(JSON.stringify(numeric.eig(A).lambda));
+//res.write(isPositiveDefinite(A, n) ? 'pos' : 'not pos');
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var stylus = require('stylus');
-var numeric = require('numeric');
 
 var app = express();
 
@@ -23,28 +22,6 @@ app.get('/partials/:partialPath', function(req, res) {
   res.render('partials/' + req.params.partialPath);
 });
 
-app.get('/matrix/:size', function(req, res) {
-  var n = req.params.size;
-  var gen = generateMatrix(n);
-  var A = gen['A'];
-  //var b = gen['b'];
-  //res.json(A);
-  res.write(JSON.stringify(A));
-  res.write(JSON.stringify(numeric.eig(A).lambda));
-  res.write(isPositiveDefinite(A, n) ? 'pos' : 'not pos');
-  res.end();
-});
-
-app.get('/genA', function(req, res) {
-  var n = req.query.n;
-  res.json(generateMatrixA(n));
-});
-
-app.get('/genB', function(req, res) {
-  var n = req.query.n;
-  res.json(generateMatrixB(n));
-});
-
 app.get('/lab1', function(req, res) {
   res.render('lab1');
 });
@@ -60,62 +37,3 @@ app.get('*', function (req, res) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
-
-
-
-function isPositiveDefinite(A) {
-  var eigenvalues = numeric.eig(A).lambda.x;
-  var positive = 0;
-  for(var i = 0; i < eigenvalues.length; i++) {
-    if(eigenvalues[i] > 0) {
-      positive = positive + 1;
-    }
-  }
-  return (positive == eigenvalues.length);
-}
-
-function isPositiveDet(A, n) {
-  var B = Array(n);
-  for (var i = 0; i < n; i++) {
-    B[i] = Array(n);
-    for (var j = 0; j < n; j++) {
-      B[i][j] = A[i][j];
-    }
-  }
-  return (numeric.det(B) > 0);
-}
-
-function getRandInt() {
-  var val = 0;
-  var MIN = -100;
-  var MAX = 100;
-  while (val == 0) {
-    val = Math.floor(Math.random() * (MAX - MIN + 1)) + MIN;
-  }
-  return val;
-}
-
-function generateMatrixA(n) {
-  // A - симметричная и положительно определенная матрица
-  var A = new Array(n);
-  for (var i = 0; i < n; i++) {
-    A[i] = new Array(n);
-  }
-
-  for (var i = 0; i < n; i++) {
-    do {
-      for (var j = 0; j < i + 1; j++) {
-        A[i][j] = A[j][i] = getRandInt();
-      }
-    } while (!isPositiveDet(A, i + 1));
-  }
-  return A;
-}
-
-function generateMatrixB(n) {
-  var b = new Array(n);
-  for (var i = 0; i < n; i++) {
-    b[i] = getRandInt();
-  }
-  return b;
-}
